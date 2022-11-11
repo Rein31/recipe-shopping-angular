c1c81e52bb8b9e20f7e63c600949b9e818da20b9
Rei <reinaldi@wgs-id.com>
2022-11-11 14:33:07 +0700

    adding component

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css'],
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'A Test',
+      'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+    ),
+    new Recipe(
+      'A Test Recipe',
+      'A Test',
+      'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
